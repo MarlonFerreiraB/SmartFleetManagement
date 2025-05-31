a53129e8e85201d8481619142f44695f3ea524f9
Marlon Ferreira Batista <marlonfbdev@gmail.com>
2025-05-31 20:42:26 -0300

    feat: Criado a estrutura

--- src/com/frota/model/entities/Caminhao.java
+++ src/com/frota/model/entities/Caminhao.java
@@ -1,4 +1,33 @@
 package com.frota.model.entities;
 
-public class Caminhao {
+import com.frota.enums.TipoCombustivel;
+
+import java.text.DecimalFormat;
+
+public class Caminhao extends  Veiculo {
+    private double capacidadeTon;
+    private int eixos;
+    private int consumoKML;
+
+    public Caminhao(String marca, String modelo, int ano, double quilometrageOuHorasUso, TipoCombustivel tipoCombustivel, double capacidadeTon, int eixos, int consumoKML) {
+        super(marca, modelo, ano, quilometrageOuHorasUso, tipoCombustivel);
+        this.capacidadeTon = capacidadeTon;
+        this.eixos = eixos;
+        this.consumoKML = consumoKML;
+    }
+
+    @Override
+    public String calcularConsumoComustivel(double distancia){
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+        String df = decimalFormat.format(distancia / this.consumoKML);
+        return df;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "capacidadeTon=" + capacidadeTon +
+                ", eixos=" + eixos +
+                ", consumoKML=" + consumoKML;
+    }
 }
