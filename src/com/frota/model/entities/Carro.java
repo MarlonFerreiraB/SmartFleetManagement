a6d1b3161de930afc77f3a281464b32473c9b6eb
Marlon Ferreira Batista <marlonfbdev@gmail.com>
2025-05-31 19:31:54 -0300

    feat: Realizado construcão da class

--- src/com/frota/model/entities/Carro.java
+++ src/com/frota/model/entities/Carro.java
@@ -1,4 +1,48 @@
 package com.frota.model.entities;
 
-public class Carro {
+import com.frota.enums.TipoCombustivel;
+
+import java.text.DecimalFormat;
+
+public class Carro extends Veiculo{
+    private TipoCombustivel tipoCombustivel;
+    private int consumoKML;
+    private int numeroPortas;
+
+    public Carro(int quantidadeLitros){this.quantidadeLitros = quantidadeLitros;}
+
+    public Carro(String marca, String modelo,int ano, double quilometrageOuHorasUso, TipoCombustivel tipoCombustivel, int consumoKML, int numeroPortas){
+        super(marca, modelo, ano, quilometrageOuHorasUso);
+        this.tipoCombustivel = tipoCombustivel;
+        //colocar um erro o tanque de uma moto cabe entre 40 a 115
+        this.consumoKML = consumoKML;
+        this.numeroPortas = numeroPortas;
+    }
+
+    public TipoCombustivel getTipoCombustivel() {
+        return tipoCombustivel;
+    }
+
+    public int getconsumoKML() {
+        return consumoKML;
+    }
+
+    public int getNumeroPortas() {
+        return numeroPortas;
+    }
+
+    @Override
+        public String calcularConsumoComustivel(double distancia){
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+        String df = decimalFormat.format(distancia / this.consumoKML);
+        return df;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +  '\'' +
+                "tipoCombustivel=" + tipoCombustivel + '\'' +
+                ", consumoKML=" + consumoKML + '\'' +
+                ", numeroPortas=" + numeroPortas;
+    }
 }
