21ad0514428515b9217812942c9f00022669c9fb
Marlon Ferreira Batista <marlonfbdev@gmail.com>
2025-05-31 17:19:28 -0300

    feat: criação da SUPERCLASS + estutura

--- src/com/frota/model/entities/Veiculo.java
+++ src/com/frota/model/entities/Veiculo.java
@@ -1,4 +1,59 @@
 package com.frota.model.entities;
 
-public class Veiculo {
+import com.frota.enums.StatusVeiculos;
+
+import java.util.UUID;
+
+public  class Veiculo {
+    protected String id = UUID.randomUUID().toString();
+    protected String marca, modelo;
+    protected int ano;
+    protected double quilometrageOuHorasUso;
+
+    public Veiculo(){}
+
+    public Veiculo( String marca, String modelo,int ano, double quilometrageOuHorasUso){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.quilometrageOuHorasUso = quilometrageOuHorasUso;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public double getQuilometrageOuHorasUso() {
+        return quilometrageOuHorasUso;
+    }
+
+    public double calcularConsumoComustivel(double distancia){
+        return distancia;
+    }
+
+    public void atualizarStatus(StatusVeiculos novoStatus){
+        System.out.println("Veiculo " + novoStatus.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "Veiculo =" +
+                "id='" + id + '\'' +
+                ", marca='" + marca + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", ano=" + ano +
+                ", quilometrageOuHorasUso=" + quilometrageOuHorasUso;
+    }
 }
