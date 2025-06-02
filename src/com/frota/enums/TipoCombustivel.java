a7684a0f7c1748f4b897ecc2acc156579b8dddab
Marlon Ferreira Batista <marlonfbdev@gmail.com>
2025-06-02 20:10:25 -0300

    feat / refactor: Modificado e implementado a interface ImplementCalcCombustivel

--- src/com/frota/enums/TipoCombustivel.java
+++ src/com/frota/enums/TipoCombustivel.java
@@ -1,10 +1,48 @@
 package com.frota.enums;
 
-public enum TipoCombustivel {
-    GASOLINA,
-    DISEL,
-    ETANOL,
-    GNV,
-    FLEX,
-    ELETRICO
+import com.frota.interfaces.ImplementCalcCombustivel;
+
+public enum TipoCombustivel implements ImplementCalcCombustivel {
+    GASOLINA{
+        @Override
+        public double calculaGastoCombustivel( int distanciaPecorrida, double totalLitros) {
+            return distanciaPecorrida/ totalLitros;
+        }
+    },
+    DISEL{
+        @Override
+        public double calculaGastoCombustivel(int distanciaPecorrida, double totalLitros) {
+            double calculo = distanciaPecorrida / totalLitros * 0.2;
+
+            return  (distanciaPecorrida / totalLitros) + calculo ;
+        }
+
+
+    },
+    ETANOL{
+        @Override
+        public double calculaGastoCombustivel(int distanciaPecorrida, double totalLitros) {
+            double calculo = ( distanciaPecorrida / totalLitros) * 0.13;
+
+            return (distanciaPecorrida/ totalLitros) - calculo ;
+        }
+    },
+    GNV{
+        @Override
+        public double calculaGastoCombustivel( int distanciaPecorrida, double totalLitros) {
+            double calculo = (distanciaPecorrida / totalLitros) * 0.25;
+
+            return (distanciaPecorrida / totalLitros) - calculo;
+        }
+    },
+    ELETRICO{
+        @Override
+        public double calculaGastoCombustivel(int distanciaPecorrida, double totalLitros) {
+            return distanciaPecorrida / totalLitros;
+        }
+    }
+
+
+
+    //implementar uma nova interface estrategia de combustivel, ela vai ter um metodo de calcular consumo e cada enum ira ter o metodo retornando o valor(verificar se tem como)
 }
