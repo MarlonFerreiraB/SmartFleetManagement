2aae24cf2c7088b660738ac1ed42b62633b9864a
Marlon Ferreira Batista <marlonfbdev@gmail.com>
2025-06-06 18:08:36 -0300

    feat: criado um metodo set para mudança de QuilometragemOuHorasUso + ciriação da lista de CustosOperacionais onde foi criado tambem um metodo para add nele.

--- src/com/frota/model/entities/Veiculo.java
+++ src/com/frota/model/entities/Veiculo.java
@@ -4,6 +4,9 @@ import com.frota.enums.StatusVeiculos;
 import com.frota.enums.TipoCombustivel;
 
 import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public  class Veiculo {
@@ -13,6 +16,7 @@ public  class Veiculo {
     protected double quilometrageOuHorasUso;
     protected TipoCombustivel tipoCombustivel;
     protected StatusVeiculos statusVeiculos;
+    private List<CustoOperacional> operacionalList = new ArrayList<>();
 
     public Veiculo(){}
 
@@ -44,6 +48,14 @@ public  class Veiculo {
         return quilometrageOuHorasUso;
     }
 
+    public List<CustoOperacional> getOperacionalList() {
+        return operacionalList;
+    }
+
+    public void setQuilometrageOuHorasUso(double quilometrageOuHorasUso) {
+        this.quilometrageOuHorasUso = quilometrageOuHorasUso;
+    }
+
     public String calcularConsumoComustivel(int distancia, double totalDeCombustivel){
         DecimalFormat decimalFormat = new DecimalFormat("#.##");
         String df = decimalFormat.format(tipoCombustivel.calculaGastoCombustivel(distancia, totalDeCombustivel));
@@ -52,6 +64,11 @@ public  class Veiculo {
 
     public void atualizarStatus(StatusVeiculos novoStatus){
         System.out.println("Veiculo " + novoStatus.toString());
+    }
+
+    public void addCusto(LocalDate data, String descricao, String tipoCusto, double valor){
+        CustoOperacional custoOperacional = new CustoOperacional(data, descricao, tipoCusto, valor,this);
+        operacionalList.add(custoOperacional);
     }
 
     @Override
